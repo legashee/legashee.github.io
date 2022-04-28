6bc42b3cdf72ce42db24ab18a9bf6d3c3a0f7056
legashee <raybox1995@gmail.com>
2022-04-28 23:38:14 +0500

    adaptive

--- smartbasket/php/config.php
+++ smartbasket/php/config.php
@@ -3,16 +3,15 @@
 
 		// *** SMTP *** //
 
-		  require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/smtp.php');
-		   const HOST = 'smtp.yandex.ru';
-		   const LOGIN = 'geramsevoyan@yandex.ru';
-		  const PASS = '332332123g';
-		   const PORT = '465';
+		require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/smtp.php');
+		const HOST = 'smtp.mail.ru';
+		const LOGIN = 'sevoyan0202@mail.ru';
+		const PASS = '332332123geram';
+		const PORT = 465;
 
 		// *** /SMTP *** //
-   
-    const SENDER = 'geramsevoyan@yandex.ru';
-    const CATCHER = 'R.inark@yandex.ru';
+    const SENDER = ('sevoyan0202@mail.ru');
+    const CATCHER = ('R.inark@yandex.ru');
     const SUBJECT = 'Заявка с сайта';
     const CHARSET = 'UTF-8';
-    
+    ?>
